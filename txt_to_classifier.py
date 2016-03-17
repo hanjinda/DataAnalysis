b4d14c15e56856a4bf19db912049c68b7f1d444c
Han <hanjinda@users.noreply.github.com>
2016-03-16 21:57:51 -0500

    Added files via upload
    add function to build basic network

--- txt_to_classifier.py
+++ txt_to_classifier.py
@@ -16,6 +16,7 @@ file_to_write_diag = "3-diagnosis.txt"
 file_to_write_treat = "4-treatment.txt"
 file_to_write_symp = "5-symptoms.txt"
 file_to_write_causes = "6-causes.txt"
+file_to_form_basic_network = "network_basic.txt"
 file_line_num = 1
 # open all output files
 f_out_1 = open(file_out_dir + "1-name.txt","w")
@@ -24,6 +25,7 @@ f_out_3 = open(file_out_dir + "3-diagnosis.txt","w")
 f_out_4 = open(file_out_dir + "4-treatment.txt","w")
 f_out_5 = open(file_out_dir + "5-symptoms.txt","w")
 f_out_6 = open(file_out_dir + "6-causes.txt","w")
+f_out = open(file_out_dir + "network_basic.txt","w")
 
 # open the file with read only permit
 for num in range (file_start, file_end):
@@ -35,43 +37,58 @@ for num in range (file_start, file_end):
 			line = line.split("name: ")
 			print line[1]
 			f_out_1.write(str(file_line_num) + "\t" +line[1])
+			# if line[1] != "":
+			# 	f_out.write(str(file_line_num) +"\t")
 
 		if "Definition: " in line:
 			line = line.split("Definition: ")
 			print line[1]
 			f_out_2.write(str(file_line_num + 10000) + "\t" +line[1])
+			if line[1] != "":
+				f_out.write(str(file_line_num) +"\t" + str(file_line_num + 10000) +"\n")
 
 		if "Symptoms and Signs: " in line:
 			line = line.split("Symptoms and Signs: ")
 			print line[1]
 			f_out_3.write(str(file_line_num + 20000) + "\t" +line[1])
+			if line[1] != "":
+				f_out.write(str(file_line_num) +"\t" + str(file_line_num + 20000) +"\n")
 
 		if "Treatment: " in line:
 			line = line.split("Treatment: ")
 			print line[1]
 			f_out_4.write(str(file_line_num + 30000) + "\t" +line[1])
+			if line[1] != "":
+				f_out.write(str(file_line_num) +"\t" + str(file_line_num + 30000) +"\n")
 
 		if "Causes: " in line:
 			line = line.split("Causes: ")
 			print line[1]
 			f_out_5.write(str(file_line_num + 40000) + "\t" +line[1])
+			if line[1] != "":
+				f_out.write(str(file_line_num) +"\t" + str(file_line_num + 40000) +"\n")
 
 		if "Diagnosis: " in line:
 			line = line.split("Diagnosis: ")
 			print line[1]
 			f_out_6.write(str(file_line_num + 50000) + "\t" +line[1])
+			if line[1] != "":
+				f_out.write(str(file_line_num) +"\t" + str(file_line_num + 50000) +"\n")
 
+
+		#f_out.write("\n")
 	# for here if you want continuous number from file 1 to the end, 
 	# then you need to separate the for loop and give each for loop a condition, 
 	# and add in each loop if line[1] != "", then file_line_num += 1
 
 	#end for
-
+	
 
 	file_line_num += 1
 	f.close
 
 #end for
+f_out.close
 f_out_1.close
 f_out_2.close
 f_out_3.close
