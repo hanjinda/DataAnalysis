2cbfef3d09d1fceff7ff9caa1cbc0abbeb82bd09
Han <han@wirelessprvnat-172-17-62-74.near.illinois.edu>
2016-03-27 22:22:10 -0500

    correction

--- json_reader.py
+++ json_reader.py
@@ -1,7 +1,27 @@
+#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 """
-	Jinda Han @ March 16, 2015
-	reading JSON files
-
+	Jinda Han @ March 27, 2015
+	reading and using JSON files
+	You may ask questions on:
+		how to print all the element from json in python?
+		Iterating and printing JSON objects in Python?
+		How to print particular JSON value in Python?
 """
+import json
+from pprint import pprint
+
+
+if __name__ == '__main__':
+	with open('classes_config.json') as data_file:    
+		classes_config = json.load(data_file)
+	for each_class in classes_config["classes"]:
+		print each_class
+		for each_class_element in classes_config["classes"][each_class]:
+			print "\t" + each_class_element			
+			if each_class_element == "keywords":
+				for each_keywords in classes_config["classes"][each_class]["keywords"]:
+					print "\t\t" + each_keywords
+
+	# print(data)
 
