d8971c3831c9748cbc4651ec9d6d81cd218905fe
Han <han@wirelessprvnat-172-17-62-74.near.illinois.edu>
2016-03-27 22:44:04 -0500

    papapa

--- py_links.py
+++ py_links.py
@@ -2,5 +2,11 @@
 # -*- coding: UTF-8 -*-
 """
 	Jinda Han @ March 27, 2015
-  How to play around with links in python
+	How to play around with links in python
 """
+
+if __name__ == '__main__':
+	folders = ["external", "ours", "testing"]
+
+	for each_folder in folders:
+		print "Currently you are in folder: " + each_folder
